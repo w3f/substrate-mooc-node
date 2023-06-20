e151a57f0d85195101489bb8dd32994d05d27d00
Bader Youssef <ibnbassem@gmail.com>
2023-06-20 13:27:33 -0400

    remove benchmarks for now

--- pallets/connect/src/benchmarking.rs
+++ pallets/connect/src/benchmarking.rs
@@ -1,11 +1,1 @@
-//! Benchmarking setup for pallet-connect
-#![cfg(feature = "runtime-benchmarks")]
-use super::*;
 
-#[allow(unused)]
-use crate::Pallet as Template;
-use frame_benchmarking::v2::*;
-use frame_system::RawOrigin;
-
-#[benchmarks]
-mod benchmarks {}
